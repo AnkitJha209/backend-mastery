e8f2bd3e2e90927e12ab5d0205b27dc5df6aae93
Ankit Jha <professionalaj209@gmail.com>
2024-11-21 20:04:01 +0530

    data-modelling

---
+++ 05_data_modelling/models/todos/sub_todo.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const subTodoSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        complete: {
+            type: Boolean,
+            deafault: false,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },{ timestamps: true}
+)
+
+export const SubTodo = mongoose.model("SubTodo", subTodoSchema)
