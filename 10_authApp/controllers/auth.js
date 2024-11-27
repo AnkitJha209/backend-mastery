1e250c5da9af148604cf0e89901b595d5a3474ba
Ankit Jha <professionalaj209@gmail.com>
2024-11-27 14:45:50 +0530

    login jwt

--- 10_authApp/controllers/auth.js
+++ 10_authApp/controllers/auth.js
@@ -1,17 +1,23 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
 
 export const signUp = async (req, res) => {
   try {
     const { username, email, password, role } = req.body;
     //check if user already exist
 
-    const existingUser = await User.findOne({ email: email })
+    const existingUser = await User.findOne({ email: email });
     if (existingUser) {
-        return res.status(404).json({
-            success: false,
-            msg: "User Already Exist",
-        });
+      return res.status(404).json({
+        success: false,
+        msg: "User Already Exist",
+      });
     }
     let hashPassword;
     try {
@@ -37,12 +43,65 @@ export const signUp = async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({
-        success: false,
-        msg: "Internal Server Error"
-    })
+      success: false,
+      msg: "Internal Server Error",
+    });
   }
 };
 
 export const logIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        msg: "Please fill all the details",
+      });
+    }
 
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(402).json({
+        success: false,
+        msg: "No User Found",
+      });
+    }
+
+    //verfiying password and generating JWT token
+    const payload = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+    if (await bcrypt.compare(password, user.password)) {
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+      user = user.toObject();
+      user.token = token
+      user.password = 'undefined'
+      
+      const options = {
+        expires: new Date( Date.now() + 3* 24 *60 * 60 * 1000),
+        httpOnly: true
+      }
+
+      res.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        msg: "User Logged in Successfully"
+      })
+    } else {
+      return res.status(404).json({
+        success: false,
+        msg: "Password does not match",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: "Internal Server Error",
+    });
+  }
 };
--- 10_authApp/routes/route.js
+++ 10_authApp/routes/route.js
@@ -4,4 +4,4 @@ import { logIn, signUp } from "../controllers/auth.js";
 export const router = express.Router();
 
 router.post('/sign-up',signUp)
-router.post('log-in',logIn)
+router.post('/log-in',logIn)
